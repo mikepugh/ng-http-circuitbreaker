eb489a874ce7fb75149d627b1a259f75969ae21f
mikepugh <mikepugh82@gmail.com>
2013-11-07 15:25:27 -0500

    Adding grunt build tasks
    jshint, uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,48 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-karma');
-
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.initConfig({
-
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+        dist: {
+            files: {
+                'dist/<%= pkg.name %>.min.js': ['app/scripts/ng-http-circuitbreaker.js']
+            }
+        }
+    },
+    copy: {
+        main: {
+            files: [
+                {src: ['app/scripts/ng-http-circuitbreaker.js'], dest: 'dist/<%=pkg.name %>.js' },
+            ]
+        }
+    },
+    jshint: {
+        options: {
+            smarttabs: true,
+            curly: true,
+            eqeqeq: true,
+            immed: true,
+            newcap: true,
+            noarg: true,
+            sub: true,
+            eqnull: true,
+            unused: true,
+            browser: true,
+            validthis: true,
+            strict: true,
+            latedef: true,
+            globals: {
+                angular: true
+            }
+        },
+        source: {
+            src: ['app/scripts/ng-http-circuitbreaker.js']
+        }
+    },
     karma: {
       unit: {
         configFile: './test/karma-unit.conf.js',
@@ -32,12 +71,14 @@ module.exports = function(grunt) {
   });
 
   //single run tests
-  grunt.registerTask('test', ['karma:unit']);
+  grunt.registerTask('test', ['jshint','karma:unit']);
   //coverage testing
-  grunt.registerTask('test:coverage', ['karma:unit_coverage']);
+  grunt.registerTask('test:coverage', ['jshint','karma:unit_coverage']);
   //autotest and watch tests
-  grunt.registerTask('autotest', ['karma:unit_auto']);
+  grunt.registerTask('autotest', ['jshint','karma:unit_auto']);
 
+
+  grunt.registerTask('build', ['jshint','karma:unit','copy:main','uglify']);
 
   //defaults
   grunt.registerTask('default', ['test']);
